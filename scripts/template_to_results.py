fbb483fb99743388f730068c83053dbc59a63cbe
KV Zhao <CLT\kv_zhao@rdai-dl-dt1.clt.gocyberlink.com>
2020-02-03 17:11:46 +0800

    Add id name to result container

--- scripts/template_to_results.py
+++ scripts/template_to_results.py
@@ -1,12 +1,14 @@
 import os
 import sys
 import json
+import time
 import struct
 from os.path import join, basename, dirname
 
 import numpy as np
 import pandas as pd
 
+import feature_analyzer.data_tools.dftools as dftools
 from feature_analyzer.data_tools.result_container import ResultContainer
 
 
@@ -16,31 +18,82 @@ def load_json(path):
     return d
 
 
-def create_event(pair_list):
+def create_event(pair_list, meta=None):
     id_pair = []
     event_pair = []
     id_A, id_B = [], []
+    id_A_name, id_B_name = [], []
     img_A, img_B = [], []
     inst_A, inst_B = [], []
     gt_lm_A, gt_lm_B = [], []
     pd_lm_A, pd_lm_B = [], []
     scores = []
-    for pair in pair_list:
-        id_a = pair['file'][0].split('/')[0]
-        id_b = pair['file'][1].split('/')[0]
-        id_pair.append(frozenset([id_a, id_b]))
-        event_pair.append(frozenset(pair['file']))
-        id_A.append(id_a)
-        img_A.append(pair['file'][0].split('/')[1])
-        gt_lm_A.append(pair['lm_gt'][0])
-        pd_lm_A.append(pair['lm_pred'][0])
-        id_B.append(id_b)
-        img_B.append(pair['file'][1].split('/')[1])
-        inst_A.append(pair['file'][0])
-        inst_B.append(pair['file'][1])
-        gt_lm_B.append(pair['lm_gt'][1])
-        pd_lm_B.append(pair['lm_pred'][1])
-        scores.append(pair['score'])
+
+    if meta is None:
+        for pair in pair_list:
+            id_a = pair['file'][0].split('/')[0]
+            id_b = pair['file'][1].split('/')[0]
+            id_pair.append(frozenset([id_a, id_b]))
+            event_pair.append(frozenset(pair['file']))
+            id_A.append(id_a)
+            img_A.append(pair['file'][0].split('/')[1])
+            gt_lm_A.append(pair['lm_gt'][0])
+            pd_lm_A.append(pair['lm_pred'][0])
+            id_B.append(id_b)
+            img_B.append(pair['file'][1].split('/')[1])
+            inst_A.append(pair['file'][0])
+            inst_B.append(pair['file'][1])
+            gt_lm_B.append(pair['lm_gt'][1])
+            pd_lm_B.append(pair['lm_pred'][1])
+            scores.append(pair['score'])
+    else:
+        for pair in pair_list:
+            id_a = pair['file'][0].split('/')[0]
+            id_b = pair['file'][1].split('/')[0]
+            id_pair.append(frozenset([id_a, id_b]))
+            event_pair.append(frozenset(pair['file']))
+            id_A.append(id_a)
+            img_A.append(pair['file'][0].split('/')[1])
+            gt_lm_A.append(pair['lm_gt'][0])
+            pd_lm_A.append(pair['lm_pred'][0])
+            id_B.append(id_b)
+            img_B.append(pair['file'][1].split('/')[1])
+            inst_A.append(pair['file'][0])
+            inst_B.append(pair['file'][1])
+            gt_lm_B.append(pair['lm_gt'][1])
+            pd_lm_B.append(pair['lm_pred'][1])
+            scores.append(pair['score'])
+
+            id_a_name = meta[meta.Class_ID == id_a].Name
+            id_b_name = meta[meta.Class_ID == id_b].Name
+
+            id_a_name = str(id_a_name.values[0]) if not id_a_name.empty else ''
+            id_b_name = str(id_b_name.values[0]) if not id_b_name.empty else ''
+
+            id_a_name = id_a_name.replace('"', '')
+            id_b_name = id_b_name.replace('"', '')
+
+            id_A_name.append(id_a_name)
+            id_B_name.append(id_b_name)
+
+    if meta is not None:
+        return pd.DataFrame({
+            'id_pair': id_pair,
+            'event_pair': event_pair,
+            'id_A': id_A,
+            'id_B': id_B,
+            'id_A_name': id_A_name,
+            'id_B_name': id_B_name,
+            'img_A': img_A,
+            'img_B': img_B,
+            'inst_A': inst_A,
+            'inst_B': inst_B,
+            'gt_lm_A': gt_lm_A,
+            'gt_lm_B': gt_lm_B,
+            'pd_lm_A': pd_lm_A,
+            'pd_lm_B': pd_lm_B,
+            'score': scores,
+        })
 
     return pd.DataFrame({
         'id_pair': id_pair,
@@ -81,17 +134,22 @@ def main(args):
 
     if args.template_folder is None:
         return
+    start = time.time()
     fp_list = load_json(join(args.template_folder, 'false_positive_list.json'))
     fn_list = load_json(join(args.template_folder, 'false_negative_list.json'))
     noface_list = load_json(join(args.template_folder, 'no_face_img_list.json'))
     wronglm_dict = load_json(join(args.template_folder, 'wrong_landmark_img_list.json'))
 
+    meta = dftools.load(args.meta_data_path)
 
     os.makedirs(args.result_container_folder, exist_ok=True)
 
-    fp_events = create_event(fp_list)
-    fn_events = create_event(fn_list)
+    fp_events = create_event(fp_list, meta)
+    print('fp events created.')
+    fn_events = create_event(fn_list, meta)
+    print('fn events created.')
     lm_events = create_lm_failure(wronglm_dict)
+    print('lm events created.')
 
     fp_rc = ResultContainer()
     fn_rc = ResultContainer()
@@ -107,6 +165,10 @@ def main(args):
     fp_rc.save(fp_rc_path)
     fn_rc.save(fn_rc_path)
     lm_rc.save(lm_rc_path)
+    end = time.time()
+    hours, rem = divmod(end - start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
     print('Done.')
 
 
